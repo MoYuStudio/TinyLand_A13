d8f79bc2e8d48a532790be5cdb0a7624e8c67070
WilsonVinson <cprewilsonvinson@gmail.com>
2023-02-23 11:56:30 +0800

    main

--- project/source/drive/csv.gd
+++ project/source/drive/csv.gd
@@ -1,16 +1,25 @@
 
+# ChatGPT
+# 目前读取正常,写入格式有误
+
+# 在Godot 4.0中读写csv文件的通用模块
+
 extends Node
 
-class csv文件处理:
+# 读取CSV文件
+func read_csv_file(file_path: String) -> Array:
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	var csv_data = []
+	var line = ""
+	while !file.eof_reached():
+		line = file.get_line()
+		csv_data.append(line.split(","))
+	file.close()
+	return csv_data
 
-	func _ready():
-		读取()
-
-	func _process(delta):
-		pass
-
-	func 读取():
-		var 文件 = FileAccess.open('res://resource/data/1.csv', FileAccess.READ)
-		var content = 文件.get_as_text()
-		print(content)
-		return content
+# 写入CSV文件
+func write_csv_file(file_path: String, data: Array) -> void:
+	var file = FileAccess.open(file_path, FileAccess.WRITE)
+	for row in data:
+		file.store_line(row.join(","))
+	file.close()
